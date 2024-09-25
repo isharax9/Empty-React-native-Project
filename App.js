a9143d882db09d992b4b330c532e5b9eb4a2789c
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-25 12:24:31 +0530

    basic method of using stylesheet

--- App.js
+++ App.js
@@ -1,23 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 
-export default App = () => {
+export default function App() {
   return (
     <View>
-      <Text style = {stylesheet.text1}>Hello World</Text>
+      <Text style={stylesheet.text1}>Hello!</Text>
     </View>
   );
-};
+}
 
-/**
- * Defines the styles for the main container view of the app.
- * The container has a flex layout, a white background color, and centers its child elements both horizontally and vertically.
- */
 const stylesheet = StyleSheet.create({
   text1: {
-    color: "blue",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-
+    color: "red"
+  }
 });
