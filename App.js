b32aaf27a25d8560b17863dacd7ae3ddfa64b8ef
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-25 11:26:12 +0530

    basic ui rendering

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
+import React from "react";
 
-export default function App() {
+export default App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello Ishara... <></></Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Hello World</Text>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
