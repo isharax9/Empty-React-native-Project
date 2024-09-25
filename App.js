c4fb0fca3f9a5c854588455b370e802f5ea5bde8
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-25 11:53:43 +0530

    external styling

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default App = () => {
   return (
     <View>
-      <Text style = {{color:"blue"}}>Hello World</Text>
+      <Text style = {stylesheet.text1}>Hello World</Text>
     </View>
   );
 };
+
+/**
+ * Defines the styles for the main container view of the app.
+ * The container has a flex layout, a white background color, and centers its child elements both horizontally and vertically.
+ */
+const stylesheet = StyleSheet.create({
+  text1: {
+    color: "blue",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+});
