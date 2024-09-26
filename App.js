ca0910d48be51411fa6a14630558f8bbd5e666b8
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-26 09:25:18 +0530

    2 button and 3 button alerts

--- App.js
+++ App.js
@@ -1,32 +1,45 @@
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Button, Alert } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const createTwoButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+
+  const createThreeButtonAlert = () =>
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      { text: 'OK', onPress: () => console.log('OK Pressed') },
+    ]);
+
   return (
-    <View style={stylesheet.view1}>
-      <Text style={stylesheet.text1}>Hello World</Text>
-      <Button
-        title="Press Me"
-        onPress={() => {
-          console.log("Hello World");
-          Alert.alert("Alert Title","Hello Ishara , How are you?");
-        }}
-      />
+    <View style={styles.container}>
+      <Button title={'2-Button Alert'} onPress={createTwoButtonAlert} />
+      <Button title={'3-Button Alert'} onPress={createThreeButtonAlert} />
     </View>
   );
-}
+};
 
-
-const stylesheet = StyleSheet.create({
-  text1: {
-    fontSize: 20,
-    color: "red",
-    fontWeight: "bold",
-  },
-
-  view1: {
-    backgroundColor: "#fff",
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  }
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
 });
+
+export default App;
