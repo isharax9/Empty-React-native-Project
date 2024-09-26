396b211b723d663547705d219118103b0b8ab8f9
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-26 12:44:01 +0530

    fix the text and button as a single row

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Component1 } from "./components/Component1";
 export default function App() {
   return (
     <View style={stylesheet.view1}>
-      <Component1 fname={"Sahan"} lname={"Perera"} />
+      <Component1 fname={"First Name"} lname={"Last Name"} />
       <Component1 fname={"Gayan"} lname={"Danushka"} />
     </View>
   );
--- components/Component1.js
+++ components/Component1.js
@@ -1,20 +1,18 @@
 import { useState } from "react";
-
 import { Button, StyleSheet, View, Text } from "react-native";
 
 export function Component1(props) {
     const [getName, setName] = useState(props.fname + " " + props.lname);
 
     return (
-        <View style={StyleSheet.view1}>
-            <Text style={StyleSheet.text1}>{getName}</Text>
+        <View style={stylesheet.container}>
+            <Text style={stylesheet.text}>{getName}</Text>
             <Button title="Change" onPress={() => { setName("Ishara lakshitha"); }} />
         </View>
     );
 }
 
-
-const styles = StyleSheet.create({
+const stylesheet = StyleSheet.create({
     container: {
         flexDirection: 'row',
         alignItems: 'center',
